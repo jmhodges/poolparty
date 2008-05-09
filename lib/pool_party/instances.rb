dc9fa0592ccb8023f52c1a9c812006a6491a3ddb
auser <auser@ausers-macbook-pro.local>
2008-05-09 16:38:29 -0700

    Added isntances into dir

---
+++ lib/pool_party/instances.rb
@@ -0,0 +1,24 @@
+module PoolParty
+  extend self
+  
+  module Instances
+    
+    class Monitor
+      attr_accessor :maximum, :minimum
+      
+      def initialize(min, max)
+        @minimum = min
+        @maximum = max
+      end
+      # Monitor on specific monitors
+      def monitor!
+      end
+    end
+    
+  end
+  
+end
+
+Dir["#{File.dirname(__FILE__)}/monitors/*"].each do |file|
+  require file
+end
