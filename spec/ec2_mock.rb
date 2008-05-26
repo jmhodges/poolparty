6aab9af936aaefe47e8931c60bba69d9b76358e1
ari <ari.lerner@citrusbyte.com>
2008-05-26 02:28:22 -0700

    Adding ec2_mock to stub ec2 calls before my computer explodes Starting to mock the remoting ** TODO: Add scaling, add heartbeat failovers **

---
+++ spec/ec2_mock.rb
@@ -0,0 +1,53 @@
+module PoolParty
+  module EC2Mock
+    def launch_new_instance!
+      h = {:instance_id => "i-58ba56c", :ip => "ip-127-0-0-1.aws.amazonaws.com", :status => "pending", :launching_time => Time.now }
+      instances << h      
+      Thread.new {wait 0.5;h[:status] = "running"} # Simulate the startup time
+      return h
+    end
+    # Shutdown the instance by instance_id
+    def terminate_instance!(instance_id)
+      instances.select {|a| a[:instance_id] == instance_id}[0][:status] = "terminating"
+    end
+    # Instance description
+    def describe_instance(id)
+      item = instances.select {|a| a[:instance_id] == id}[0]
+      EC2ResponseObject.get_hash_from_response(item)
+    end
+    # Get instance by id
+    def get_instance_by_id(id)
+      get_instances_description.select {|a| a.instance_id == id}[0] rescue nil
+    end
+    # Get the s3 description for the response in a hash format
+    def get_instances_description
+      instances
+    end
+    def ec2
+      @ec2 ||= EC2::Base.new(:access_key_id => "not a key", :secret_access_key => "not a key")
+    end
+    def instances
+      @instances ||= []
+    end
+  end
+  class EC2ResponseObject
+    def self.get_descriptions(resp)
+      rs = resp.DescribeInstancesResponse.reservationSet.item
+      rs = rs.respond_to?(:instancesSet) ? rs.instancesSet : rs
+      out = begin
+        rs.reject {|a| a.empty? }.collect {|r| EC2ResponseObject.get_hash_from_response(r.instancesSet.item)}.reject {|a| a.nil?  }
+      rescue Exception => e
+        begin
+          # Really weird bug with amazon's ec2 gem
+          rs.reject {|a| a.empty? }.collect {|r| EC2ResponseObject.get_hash_from_response(r)}.reject {|a| a.nil?  }
+        rescue Exception => e
+          []
+        end                
+      end
+      out
+    end
+    def self.get_hash_from_response(resp)
+      {:instance_id => resp.instanceId, :ip => resp.dnsName, :status => resp.instanceState.name, :launching_time => resp.launchTime} rescue nil
+    end
+  end
+end
--- spec/remoting_spec.rb
+++ spec/remoting_spec.rb
@@ -1,91 +1,33 @@
 require File.dirname(__FILE__) + '/spec_helper'
+require File.dirname(__FILE__) + '/ec2_mock'
 
 # RUN THESE ONE AT A TIME
 module PoolParty
-  class Host
-    def on_server_exit
-      puts "Exiting #{Time.now}"; 
-    end
+  class Master
+    include EC2Mock
   end
 end
 
 describe "Actual remoting" do
   before(:each) do
-    @host, @options = PoolParty.server({:config_file => "#{File.dirname(__FILE__)}/../config/config.yml"})
-    @starting_size = @host.list_of_running_instances.size
+    Application.stub!(:minimum_instances).and_return(2)
+    Application.stub!(:maximum_instances).and_return(10)
+    Application.stub!(:verbose).and_return(false) # Turn off messaging
+    
+    @master = Master.new
   end
-  after(:each) do
-    @host.request_termination_of_all_instances
-  end
-  it "should start up the minimum instances when starting" do
-    wait_launch do
-      @host.start!
+  describe "Starting" do
+    before(:each) do
+      @master.start_cloud!
     end
-    wait "20.seconds"
-    if @starting_size > 0
-      @host.list_of_running_instances.size.should == Application.minimum_instances 
-    else
-      @host.list_of_pending_instances.size.should == Application.minimum_instances
+    it "should start the cloud with instances" do    
+      @master.list_of_instances.should_not be_empty
     end
-  end
-  it "should update_instance_values after the polling_time has passed with new RemoteInstances" do
-    wait_launch("50.seconds") do
-      @host.start!
+    it "should start the cloud with running instances" do
+      @master.list_of_running_instances.should_not be_empty
     end
-    @host.running_instances.class.should == Array
-    @host.running_instances[0].class.should == RemoteInstance
-  end
-  it "should try to add a new instance when he load is getting heavy" do    
-    @host.stub!(:global_load).and_return 0.86
-    @host.stub!(:terminate_instance_if_load_is_low).at_least(1).and_return false
-
-    Application.stub!(:polling_time).and_return 20
-    Application.stub!(:heavy_load).and_return 0.50
-    Application.stub!(:maximum_instances).and_return(2)
-    
-    @host.launch_minimum_instances
-    
-    wait "40.seconds"
-    
-    wait_launch("2.minutes") do
-      @host.start!
+    it "should start with the minimum_instances running" do
+      @master.list_of_running_instances.size.should == Application.minimum_instances
     end
-    
-    wait "20.seconds"
-    # If the instance already started, we just want to calculate the actual result, so it's either or
-    @host.list_of_running_instances.size.should == 2
-  end
-  
-  it "should not add a new instance if the load is not heavy" do    
-    @host.stub!(:global_load).and_return 0.30
-    Application.stub!(:polling_time).and_return "30.seconds"
-    Application.stub!(:heavy_load).and_return 0.80
-    
-    @host.launch_minimum_instances
-    wait "10.seconds"
-    wait_launch("60.seconds") do
-      @host.start!
-    end
-    
-    # If the instance already started, we just want to calculate the actual result, so it's either or
-    @host.list_of_running_instances.size.should == 1
-  end
-  it "should try to terminate an instance if the load is low" do
-    @host.stub!(:global_load).and_return 0.15
-    @host.should_receive(:add_instance_if_load_is_high).and_return false
-    
-    Application.stub!(:polling_time).and_return "10.seconds"
-    Application.stub!(:light_load).and_return 0.20
-    
-    @host.launch_minimum_instances
-    @host.launch_new_instance! # Force launch one
-    
-    wait "30.seconds"
-    wait_launch("120.seconds") do
-      @host.start!
-    end
-    
-    # If the instance already started, we just want to calculate the actual result, so it's either or
-    @host.list_of_running_instances.size.should == 1
   end
 end
