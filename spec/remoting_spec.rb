0d47c8f50bb82800fcec696729bbf8560af93533
ari <ari.lerner@citrusbyte.com>
2008-05-26 03:28:30 -0700

    More updating remoting_spec

--- spec/remoting_spec.rb
+++ spec/remoting_spec.rb
@@ -8,7 +8,7 @@ end
 
 describe "Master remoting: " do
   before(:each) do
-    Application.stub!(:environment).and_return("test") # So it doesn't daemonize
+    Application.stub!(:environment).and_return("development") # So it daemonizes
     Application.stub!(:minimum_instances).and_return(2)
     Application.stub!(:maximum_instances).and_return(10)
     Application.stub!(:polling_time).and_return(0.5)
@@ -33,17 +33,19 @@ describe "Master remoting: " do
   end
   describe "maintaining" do
     before(:each) do
-      @master.start_monitor!
+      Thread.new {@master.start_monitor!}
     end
     after(:all) do
       `killall -9 ruby`
     end
     it "should maintain the minimum_instances if one goes down" do
       @master.start!
+      wait 1.1
+      p Application.minimum_instances - @master.number_of_pending_and_running_instances
+      @master.terminate_instance!(@master.list_of_running_instances[0][:instance_id])      
       wait 2
-      @master.terminate_instance!(@master.list_of_running_instances[0][:instance_id])
-      wait 2
-      @master.list_of_running_instances.size.should == Application.minimum_instances
+      p Application.minimum_instances - @master.number_of_pending_and_running_instances
+      @master.number_of_pending_and_running_instances.should == Application.minimum_instances
     end
   end
 end
