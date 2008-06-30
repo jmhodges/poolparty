468c87e19d2e56b18078eb74215afb3979fb5c1f
ari <ari.lerner@citrusbyte.com>
2008-06-30 14:46:23 -0700

    Updated plugin and plugin_spec_helper

--- lib/helpers/plugin_spec_helper.rb
+++ lib/helpers/plugin_spec_helper.rb
@@ -1,36 +1,39 @@
-class PluginSpecHelper
-  def self.define_stubs(num=1)
-    define_master
-    @nodes = define_instances(num)
-    
-    @master.stub!(:execute_tasks).and_return true
-    @master.stub!(:launch_minimum_instances).and_return true
-    @master.stub!(:number_of_pending_instances).and_return 0
-    @master.stub!(:get_node).with(0).and_return @instance0
-        
-    @master.stub!(:nodes).and_return @nodes
+module PoolParty
+  class PluginSpecHelper
+    def self.define_stubs(num=1)
+      define_master
+      @nodes = define_instances(num)
 
-    Kernel.stub!(:system).and_return "true"
+      @master.stub!(:execute_tasks).and_return true
+      @master.stub!(:launch_minimum_instances).and_return true
+      @master.stub!(:number_of_pending_instances).and_return 0
+      @master.stub!(:get_node).with(0).and_return @instance0
 
-    Provider.stub!(:install_poolparty).and_return true
-    Provider.stub!(:install_userpackages).and_return true
-    
-    [@master, @nodes]
-  end
-  
-  def self.define_master
-    @master ||= Master.new
-  end
-  def self.define_instances(num)
-    returning [] do |arr|
-      num.times do |i|
-        eval <<-EOE
-          @instance#{i} = RemoteInstance.new
-          @instance#{i}.stub!(:ssh).and_return "true"
-          @instance#{i}.stub!(:scp).and_return "true"
-          @instance#{i}.stub!(:name).and_return "node#{i}"          
-        EOE
-        arr << eval("@instance#{i}")
+      @master.stub!(:nodes).and_return @nodes
+
+      Kernel.stub!(:system).and_return "true"
+
+      Provider.stub!(:install_poolparty).and_return true
+      Provider.stub!(:install_userpackages).and_return true
+
+      [@master, @nodes]
+    end
+
+    def self.define_master
+      @master ||= Master.new
+    end
+    def self.define_instances(num)
+      # Too many gross evals
+      returning [] do |arr|
+        num.times do |i|
+          eval <<-EOE
+            @instance#{i} = RemoteInstance.new
+            @instance#{i}.stub!(:ssh).and_return "true"
+            @instance#{i}.stub!(:scp).and_return "true"
+            @instance#{i}.stub!(:name).and_return "node#{i}"          
+          EOE
+          arr << eval("@instance#{i}")
+        end
       end
     end
   end
