63da7ff51c07004da33797724a71bff5cd173490
Ari Lerner <ari.lerner@citrusbyte.com>
2008-07-12 22:45:16 -0700

    Updated gemspec for github

--- lib/poolparty/tasks.rb
+++ lib/poolparty/tasks.rb
@@ -41,7 +41,7 @@ module PoolParty
       @compiled_rakefile ||= eval(rake_str.join("\n")) # Not ideal
     end
     def plugin_rakefiles
-      Dir["#{PoolParty.plugin_dir}/*/Rakefile"].each {|f| puts f }
+      Dir["#{PoolParty.plugin_dir}/*/Rakefile"].each {|f| puts open(File.join(File.expand_path(File.dirname(t)), File.basename(t))).read }
       Dir["#{PoolParty.plugin_dir}/*/Rakefile"].each {|t| rake_str << open(File.join(File.expand_path(File.dirname(t)), File.basename(t))).read }
     end
   end
