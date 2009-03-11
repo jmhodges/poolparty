817774e133a2c6d900aeb82a3d8bd8edfe1e90c6
Ari Lerner <alerner@YPCMC05591.local>
2009-03-11 13:52:45 -0700

    Fixed variable spec

--- spec/poolparty/resources/variable_spec.rb
+++ spec/poolparty/resources/variable_spec.rb
@@ -1,24 +1,26 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-include PoolParty::Resources
-
-describe "Variable" do
-  before(:each) do
-    
-    @variable = variable(:name => "name", :value => "ari Lerner")
-  end
-  it "should have a package" do
-    resource(:variable).should_not be_empty
-  end
-  it "should have the 'name' variable set as a variable" do
-    @variable.to_string.should =~ /\$name = 'ari Lerner'/
-  end
-  it "should be able to set the variable as an array if passed an array" do
-    v = variable(:name => "girlfriends", :value => %w(Rachel Erica Michelle))
-    v.to_string.should == "$girlfriends = [ 'Rachel', 'Erica', 'Michelle' ]"
-  end
-  it "should set the string to be without a string if it's a function" do
-    v = variable(:name => "girlfriends", :value => "lookup_var()")
-    v.to_string.should == "$girlfriends = lookup_var()"
+describe "Variables" do
+  describe "instances" do
+    before(:each) do
+      @tc = TestBaseClass.new do
+        has_variable(:name => "master", :value => "herman")
+      end
+      @variable = @tc.resource(:variable).first
+    end
+    it "have the name in the options" do
+      @variable.name.should == "master"
+    end
+    it "should store the owner's name as well" do
+      @variable.value.should == "herman"
+    end
+    describe "into PuppetResolver" do
+      before(:each) do
+        @compiled = PuppetResolver.new(@tc.to_properties_hash).compile
+      end
+      it "have the variable set in the puppet output" do
+        @compiled.should match(/\$master = "herman"/)
+      end
+    end
   end
 end
