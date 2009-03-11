4f70e8346d3a9f870ee66611c57916d270fc214a
fairchild <fairchild@stimble.net>
2009-03-11 15:21:28 -0700

    Remove to_option_string String specs since they are no longer used. example_spec.rb is loading examples/basic.rb cloud, but was not calling the pool(:application) specificed, but instead pool(:app) which was still passing many specs since basic.rb is so close to the defaults.

--- spec/poolparty/poolparty/example_spec.rb
+++ spec/poolparty/poolparty/example_spec.rb
@@ -5,24 +5,26 @@ describe "basic" do
   before(:each) do
     @example_dir = ::File.join(::File.dirname(__FILE__), "..", "..", "..", "examples")
     reset!
-    PoolParty::Script.inflate open(@example_dir + "/basic.rb").read
+    PoolParty::Script.inflate File.read(@example_dir + "/basic.rb")
   end
   it "should have one pool called :app" do
-    pool(:app).should_not be_nil
+    pool(:application).should_not be_nil
   end
   it "should have a cloud called :app" do
-    pool(:app).cloud(:app).should_not be_nil
+    pool(:application).cloud(:app).should_not be_nil
   end
   it "should have a cloud called :db" do
-    pool(:app).cloud(:db).should_not be_nil
+    pool(:application).cloud(:db).should_not be_nil
   end
   it "should set the minimum_instances on the cloud to 2 (overriding the pool options)" do
-    pool(:app).cloud(:app).minimum_instances.should == 2
+    pool(:application).cloud(:app).minimum_instances.should == 2
   end
   it "should set the maximum_instances on the cloud to 5" do
-    pool(:app).cloud(:app).maximum_instances.should == 5
+    pool(:application).cloud(:app).maximum_instances.should == 5
   end
   it "should set the minimum_instances on the db cloud to 3" do
-    pool(:app).cloud(:db).minimum_instances.should == 3
+    require 'rubygems'; require 'ruby-debug'; debugger
+    puts "<pre>#{cloud(:db).to_properties_hash.to_yaml}</pre>"
+    pool(:application).cloud(:db).minimum_instances.should == 3
   end
 end
