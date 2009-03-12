49a95c0942d0437f44b0f8507a95fad9ba2044f0
Ari Lerner <alerner@YPCMC05591.local>
2009-03-12 10:50:12 -0700

    Fixed cloud_resourcer_spec

--- spec/poolparty/modules/cloud_resourcer_spec.rb
+++ spec/poolparty/modules/cloud_resourcer_spec.rb
@@ -47,68 +47,25 @@ describe "CloudResourcer" do
     @tc.minimum_runtime 40.minutes
     @tc.minimum_runtime.should == 40.minutes
   end
-  describe "keypair_path" do
-    before(:each) do
-    end
-    it "should look for the file in the known directories it should reside in" do
-      @tc.should_receive(:keypair_paths).once.and_return []
-      @tc.keypair_path
-    end
-    it "should see if the file exists" do
-      @t = "#{File.expand_path(Base.base_keypair_path)}"
-      ::File.should_receive(:exists?).with(@t+"/id_rsa-rangerbob").and_return false
-      ::File.stub!(:exists?).with(@t+"/rangerbob").and_return false
-      @tc.should_receive(:keypair_paths).once.and_return [@t]
-      @tc.keypair_path
-    end
-    it "should fallback to the second one if the first doesn't exist" do
-      @t = "#{File.expand_path(Base.base_keypair_path)}"
-      @q = "#{File.expand_path(Base.base_config_directory)}"
-      ::File.stub!(:exists?).with(@t+"/id_rsa-rangerbob").and_return false
-      ::File.stub!(:exists?).with(@t+"/rangerbob").and_return false
-      ::File.stub!(:exists?).with(@q+"/id_rsa-rangerbob").and_return false
-      ::File.should_receive(:exists?).with(@q+"/rangerbob").and_return true
-      @tc.should_receive(:keypair_paths).once.and_return [@t, @q]
-      @tc.keypair_path.should == "/etc/poolparty/rangerbob"
-    end
-    describe "exists" do
-      before(:each) do
-        @t = "#{File.expand_path(Base.base_keypair_path)}"
-        ::File.stub!(:exists?).with(@t+"/id_rsa-rangerbob").and_return false
-        ::File.stub!(:exists?).with(@t+"/rangerbob").and_return true
-      end
-      it "should have the keypair_path" do
-        @tc.respond_to?(:keypair_path).should == true
-      end
-      it "should set the keypair to the Base.keypair_path" do      
-        @tc.keypair_path.should =~ /\.ec2\/rangerbob/
-      end
-      it "should set the keypair to have the keypair set" do
-        @tc.keypair.should =~ /rangerbob/
-      end
-      it "should set it to the Base keypair_path and the keypair" do
-        @tc.keypair_path.should == "#{File.expand_path(Base.base_keypair_path)}/#{@tc.keypair}"
-      end
-    end
-  end
   describe "parents" do
     before(:each) do
-      @testparent = TestParentClass.new :parent_of_bob do
+      @testparent = 
+      TestParentClass.new(:parent_of_bob) do
         test_option "blankity blank blank"
         
         ResourcerTestClass.new :bob do
         end
-      end      
-    end
-    describe "setting" do
-      it "set 1 service on the parent class" do
-        @testparent.services.size.should == 1
       end
-      it "set the service as a ResourcerTestClass named bob" do
-        @testparent.services.first.name.should == :bob
-      end
-      it "set the parent's options on the child" do
-        @testparent.services.first.test_option.should == "blankity blank blank"
+      describe "setting" do
+        it "set 1 service on the parent class" do
+          @testparent.services.size.should == 1
+        end
+        it "set the service as a ResourcerTestClass named bob" do
+          @testparent.services.first.name.should == :bob
+        end
+        it "set the parent's options on the child" do
+          @testparent.services.first.test_option.should == "blankity blank blank"
+        end
       end
     end
   end
