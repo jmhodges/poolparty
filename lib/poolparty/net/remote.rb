7354590d7c87e68090e45365f608b38e172450b8
Ari Lerner <arilerner@mac.com>
2009-03-22 14:12:53 -0700

    Removed the remote methods from the module

--- lib/poolparty/net/remote.rb
+++ lib/poolparty/net/remote.rb
@@ -16,47 +16,6 @@ end
 
 module PoolParty
   module Remote
-    module ClassMethods
-    end
-    
-    module InstanceMethods
-      def using(t)
-        @cloud = self
-        puts "--- using t: #{t} and #{self.class.available_bases.join(" ")}"
-        if t && self.class.available_bases.include?(t.to_sym)
-          unless using_remoter?
-            self.class.send :attr_reader, :remote_base
-            self.class.send :attr_reader, :parent_cloud
-            klass = "#{t}".classify.constantize
-            @remote_base = klass.send :new
-            @parent_cloud = @cloud
-          end
-        else
-          puts "Unknown remote base" 
-        end
-      end
-      
-      def using_remoter?
-        @remote_base
-      end
-      
-      # Add methods from the remote_base, ec2 for example, if the remote base has those methods, otherwise, pass up to the regular method_missing
-      def method_missing(m, *args, &block) #:nodoc:
-        if @remote_base && @remote_base.respond_to?(m)
-          @remote_base.send m, *args, &block
-        else
-          super
-        end
-      end
-      
-    end
-    
-    def self.included(receiver)
-      puts "I, #{self} WAS INCLUDED by #{receiver}"
-      receiver.extend         ClassMethods
-      receiver.send :include, InstanceMethods      
-    end
-
   end
 end
 
